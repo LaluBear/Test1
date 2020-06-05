41aa83579530c43cd4c63325596b740c6297341f
Chicken <nattapat969@gmail.com>
2020-06-05 14:51:21 +0700

    updated

--- myproject/First.py
+++ myproject/First.py
@@ -1,7 +1,9 @@
-import Flask, render_template
+from Flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+
+    name = "david"
     return render_template("hello.html", name=f"Hello, {name}!")
